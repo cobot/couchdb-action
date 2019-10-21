ac95cc836dffd5f69869c37c301034e17135b93f
Alexander Lang <alex@upstre.am>
2019-10-21 21:53:33 +0200

    more sleep

--- .github/actions/couchdb-action/entrypoint.sh
+++ .github/actions/couchdb-action/entrypoint.sh
@@ -4,7 +4,7 @@ echo "Starting Docker..."
 sh -c "docker run -d -p 5984:5984 couchdb:$INPUT_COUCHDB_VERSION"
 echo "Started Docker..."
 echo "Waiting for CouchDB..."
-sleep 10
+sleep 20
 echo "done."
 docker ps
 echo "Checking CouchDB:"
