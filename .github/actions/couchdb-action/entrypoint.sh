a80b2e4bde0fc7bc8f07c53c131db14a3ae2f859
Alexander Lang <alex@upstre.am>
2019-10-21 23:08:34 +0200

    exec

--- .github/actions/couchdb-action/entrypoint.sh
+++ .github/actions/couchdb-action/entrypoint.sh
@@ -6,9 +6,14 @@ echo "Started Docker..."
 echo "Waiting for CouchDB..."
 sleep 20
 echo "done."
+echo "DOCKER PS"
 docker ps
 export NAME=`docker ps --format "{{.Names}}" --last 1`
+echo "DOCKER TOP"
 docker top $NAME
+echo "DOCKER LOGS"
 docker logs $NAME
+echo "DOCKER EXEC"
+docker exec $NAME curl -i http://127.0.0.1:5984/
 echo "Checking CouchDB:"
-curl -i http://localhost:5986/
+curl -i http://localhost:5984/
