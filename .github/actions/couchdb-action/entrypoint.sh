ba3fce69c1064df220a95685869278034ed203d2
Alexander Lang <alex@upstre.am>
2019-10-21 21:07:55 +0200

    debug

--- .github/actions/couchdb-action/entrypoint.sh
+++ .github/actions/couchdb-action/entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+echo "Starting Docker..."
 sh -c "docker run -d -p 5984:5984 couchdb:$INPUT_COUCHDB_VERSION"
+echo "Started Docker..."
+echo "Waiting for CouchDB..."
+sleep 10
+echo "done."
