7ce29226a313b1971e4c234f0767883e0577ff88
Alexander Lang <alex@upstre.am>
2019-10-24 14:26:56 +0200

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,12 +25,13 @@ docker exec $NAME curl -sS 'http://127.0.0.1:5984/_replicator' -X PUT -H 'Conten
 
 
 # Enable Erlang query server
-if [$INPUT_ERLANG_QUERY_SERVER = true]
-then
+env
+# if [$INPUT_ERLANG_QUERY_SERVER = true]
+# then
   echo "Enabling Erlang query server..."
   docker exec $NAME echo "[native_query_servers]\nerlang = {couch_native_process, start_link, []}" >> /etc/couchdb/default.d/15-erlang-query-server.ini
   docker exec $NAME service couchdb restart
   wait_for_couchdb
-fi
+# fi
 
 echo ::set-output name=ip::$hostip
