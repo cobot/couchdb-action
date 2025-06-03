72c294d5ad8a043c982797f7eac7209e2aac92d0
Alexander Lang <alex@cobot.me>
2025-06-03 10:24:20 +0200

    remove debug, limit retries

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,25 +8,19 @@ docker run -d -p 5984:5984 -p 5986:5986 --tmpfs /ram_disk couchdb
 # CouchDB container name
 export NAME=`docker ps --format "{{.Names}}" --last 1`
 
-sleep 10
-
-echo "CouchDB version: $INPUT_COUCHDB_VERSION"
-echo "Dockerfile.couchdb: $(cat Dockerfile.couchdb)"
-echo "Name: $NAME"
-hostip=$(ip route show | awk '/default/ {print $3}')
-echo "Host IP: $hostip"
-
-curl -i http://$hostip:5984/
-
-
-docker logs $NAME
-
 wait_for_couchdb() {
   echo "Waiting for CouchDB..."
   hostip=$(ip route show | awk '/default/ {print $3}')
+  retries=0
+  max_retries=20
 
   while ! curl -f http://$hostip:5984/ &> /dev/null
   do
+    retries=$((retries+1))
+    if [ $retries -ge $max_retries ]; then
+      echo "CouchDB did not become available after $max_retries attempts. Exiting."
+      exit 1
+    fi
     echo "."
     sleep 1
   done
