a0340c4978ae2298eb8d889ce7163dc1564848bc
Alexander Lang <alex@cobot.me>
2025-06-03 10:15:27 +0200

    fix sed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "Starting Docker..."
-sh -c "sed -i '' 's/COUCHDB_VERSION/$INPUT_COUCHDB_VERSION/' Dockerfile.couchdb"
-sh -c "docker build --tag couchdb -f Dockerfile.couchdb ."
-sh -c "docker run -d -p 5984:5984 -p 5986:5986 --tmpfs /ram_disk couchdb"
+sed -i 's/COUCHDB_VERSION/$INPUT_COUCHDB_VERSION/' Dockerfile.couchdb
+docker build --tag couchdb -f Dockerfile.couchdb .
+docker run -d -p 5984:5984 -p 5986:5986 --tmpfs /ram_disk couchdb
 
 # CouchDB container name
 export NAME=`docker ps --format "{{.Names}}" --last 1`
