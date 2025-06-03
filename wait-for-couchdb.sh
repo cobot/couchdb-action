27da46efdd678580a7d03a474ed5a5fc205ce32a
Alexander Lang <alex@cobot.me>
2025-06-03 12:29:56 +0200

    try to fix wait script

--- wait-for-couchdb.sh
+++ wait-for-couchdb.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 
-wait_for_couchdb() {
-  echo "Waiting for CouchDB..."
-  retries=0
-  max_retries=20
+echo "Waiting for CouchDB..."
+retries=0
+max_retries=20
 
-  while ! curl -f http://127.0.0.1:5984/ &> /dev/null
-  do
-    retries=$((retries+1))
-    if [ $retries -ge $max_retries ]; then
-      echo "CouchDB did not become available after $max_retries attempts. Exiting."
-      exit 1
-    fi
-    echo "."
-    sleep 1
-  done
-}
-wait_for_couchdb
+until curl --output /dev/null --silent --head --fail http://127.0.0.1:5984/; do
+  retries=$((retries+1))
+  if [ $retries -ge $max_retries ]; then
+    echo "CouchDB did not become available after $max_retries attempts. Exiting."
+    exit 1
+  fi
+  echo "."
+  sleep 1
+done
 
 echo "CouchDB is up and running."
