ba8267f5814cff4939741715d691c523e481b05d
Alexander Lang <alex@cobot.me>
2025-06-03 10:04:20 +0200

    fix users

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 echo "Starting Docker..."
 sh -c "sed -i '' 's/COUCHDB_VERSION/$INPUT_COUCHDB_VERSION/' Dockerfile.couchdb"
-sh -c "docker build -l warn --tag couchdb -f Dockerfile.couchdb ."
-sh -c "docker run -l warn -d -p 5984:5984 -p 5986:5986 --tmpfs /ram_disk couchdb"
+sh -c "docker build --tag couchdb -f Dockerfile.couchdb ."
+sh -c "docker run -d -p 5984:5984 -p 5986:5986 --tmpfs /ram_disk couchdb"
 
 # CouchDB container name
 export NAME=`docker ps --format "{{.Names}}" --last 1`
@@ -35,6 +35,6 @@ wait_for_couchdb
 
 # Set up system databases
 echo "Setting up CouchDB system databases..."
-docker exec $NAME curl -sS 'http://127.0.0.1:5984/_users' -X PUT -H 'Content-Type: application/json' --data '{"id":"_users","name":"_users"}' > /dev/null
-docker exec $NAME curl -sS 'http://127.0.0.1:5984/_global_changes' -X PUT -H 'Content-Type: application/json' --data '{"id":"_global_changes","name":"_global_changes"}' > /dev/null
-docker exec $NAME curl -sS 'http://127.0.0.1:5984/_replicator' -X PUT -H 'Content-Type: application/json' --data '{"id":"_replicator","name":"_replicator"}' > /dev/null
+docker exec $NAME curl -sS 'http://admin:admin@127.0.0.1:5984/_users' -X PUT -H 'Content-Type: application/json' --data '{"id":"_users","name":"_users"}' > /dev/null
+docker exec $NAME curl -sS 'http://admin:admin@127.0.0.1:5984/_global_changes' -X PUT -H 'Content-Type: application/json' --data '{"id":"_global_changes","name":"_global_changes"}' > /dev/null
+docker exec $NAME curl -sS 'http://admin:admin@127.0.0.1:5984/_replicator' -X PUT -H 'Content-Type: application/json' --data '{"id":"_replicator","name":"_replicator"}' > /dev/null
