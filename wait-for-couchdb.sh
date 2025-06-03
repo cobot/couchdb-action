e026f5e5a6acf4d102c72765405ff2ecbd62ff7f
Alexander Lang <alex@cobot.me>
2025-06-03 12:22:02 +0200

    re-enable wait

--- wait-for-couchdb.sh
+++ wait-for-couchdb.sh
@@ -2,12 +2,10 @@
 
 wait_for_couchdb() {
   echo "Waiting for CouchDB..."
-  hostip=$(ip route show | awk '/default/ {print $3}')
-  echo "CouchDB host IP: $hostip"
   retries=0
   max_retries=20
 
-  while ! curl -f http://$hostip:5984/ &> /dev/null
+  while ! curl -f http://127.0.0.1:5984/ &> /dev/null
   do
     retries=$((retries+1))
     if [ $retries -ge $max_retries ]; then
