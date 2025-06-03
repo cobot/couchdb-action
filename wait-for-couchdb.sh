4b7acc29bc99c517c68d8e4bb92e9ee7b725d458
Alexander Lang <alex@cobot.me>
2025-06-03 11:50:52 +0200

    change to composite action

---
+++ wait-for-couchdb.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+wait_for_couchdb() {
+  echo "Waiting for CouchDB..."
+  hostip=$(ip route show | awk '/default/ {print $3}')
+  retries=0
+  max_retries=20
+
+  while ! curl -f http://$hostip:5984/ &> /dev/null
+  do
+    retries=$((retries+1))
+    if [ $retries -ge $max_retries ]; then
+      echo "CouchDB did not become available after $max_retries attempts. Exiting."
+      exit 1
+    fi
+    echo "."
+    sleep 1
+  done
+}
+wait_for_couchdb
+
+echo "CouchDB is up and running."
